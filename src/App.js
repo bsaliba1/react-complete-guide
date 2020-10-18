316ff016f6f28dc2449f9355bbaeb13dba371976
bsaliba1 <baptsal@gmail.com>
2020-10-17 20:50:47 -0400

    Added radium and editted button on hover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -47,6 +48,10 @@ class App extends Component {
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -68,6 +73,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
     }
 
     const classes = [];
@@ -92,4 +101,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
